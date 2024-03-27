943b4c8610c05cbaea004ede668002ae7821ed84
Leticia <Leticia@DESKTOP-U0NS2PU>
2024-03-27 11:50:02 +0000

    método restar del primer usuario

--- proLeticiaCynthia/src/proLeticiaCynthia/PracticaEntornos.java
+++ proLeticiaCynthia/src/proLeticiaCynthia/PracticaEntornos.java
@@ -2,6 +2,9 @@ package proLeticiaCynthia;
 
 public class PracticaEntornos {
 
+	int restar (int a, int b) {
+		return a-b;
+	}
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola nuevamente Mundo");

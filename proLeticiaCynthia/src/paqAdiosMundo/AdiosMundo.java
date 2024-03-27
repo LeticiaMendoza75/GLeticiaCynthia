252290fb37b0fede00181523e317d8beb4e5bd9d
Leticia <Leticia@DESKTOP-U0NS2PU>
2024-03-27 13:08:12 +0000

    print estamos terminando

--- proLeticiaCynthia/src/paqAdiosMundo/AdiosMundo.java
+++ proLeticiaCynthia/src/paqAdiosMundo/AdiosMundo.java
@@ -3,7 +3,7 @@ package paqAdiosMundo;
 public class AdiosMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Estamos terminando");
 
 	}
 

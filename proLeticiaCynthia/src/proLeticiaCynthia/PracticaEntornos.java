593fb2326cff9af7ce3d8aecba8c582988cd26ed
Leticia <Leticia@DESKTOP-U0NS2PU>
2024-03-27 11:52:00 +0000

    mètodo sumar del segundo  usuario

--- proLeticiaCynthia/src/proLeticiaCynthia/PracticaEntornos.java
+++ proLeticiaCynthia/src/proLeticiaCynthia/PracticaEntornos.java
@@ -2,6 +2,10 @@ package proLeticiaCynthia;
 
 public class PracticaEntornos {
 
+	int sumar (int a, int b) {
+		return a+b;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola nuevamente Mundo");
